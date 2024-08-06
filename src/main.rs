22fd8e55057a783f4b261b63d0eada01c2ad1775
Vasanth Gopal <vasanth1627@gmail.com>
2024-08-06 15:02:56 +0530

    importing the image crate, created a function to open images

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod args;
 use args::Args;
+use image::{io::Reader, DynamicImage, ImageFormat};
 
 
 
@@ -9,3 +10,12 @@ fn main() {
     println!("{:?}", args);
 }
 
+//creating a function named find_image_from_path to open the image file from a path argument
+
+fn find_image_from_path(path: String) ->(DynamicImage, ImageFormat){
+    let image_reader: Reader<BufReader<File>> = Reader::open(path).unwrap();
+    let image_format: ImageFormat = image_reader.format().unwrap();
+    let image: DynamicImage = image_reader.decode().unwrap();
+    (image, image_format)
+}
+// The image and image_format variables are returned as a tuple
